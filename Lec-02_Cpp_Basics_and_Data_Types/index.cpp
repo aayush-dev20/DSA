9d2f9880a2f0f7cf9d246a2e5d465d68501b10af
aayush-dev20 <aayushkr.devv@gmail.com>
2026-01-03 10:44:09 +0530

    DSA Lec 02

---
+++ Lec-02_Cpp_Basics_and_Data_Types/index.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+
+//     cout<< " namaste duniya :-) " <<endl  
+//     // or  we can use  \n'
+//     ;
+
+// }
+
+int main(){
+
+    int a = 123;
+
+    cout<< a <<endl; 
+    
+    char b = 'v' ;
+    cout<< b <<endl;
+
+    bool bl = true;
+    cout<< bl <<endl;
+
+    float f = 2.3;
+    cout<< f <<endl;
+
+    double d = 1.22;
+    cout<< d <<endl;
+
+   int size = sizeof(a);
+   cout<< "Size of a is :"<< size <<endl;
+
+//      int size = sizeof(d);
+//     cout << "Size of a is :" << size << endl;
+
+//     int size = sizeof(f);
+//     cout << "Size of f is :" << size << endl;
+ }
